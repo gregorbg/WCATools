33908e6907acc33a4340bbbfe19f8068f961affd
suushie_maniac <suushiemaniac@gmail.com>
2016-05-04 14:42:58 +0200

    Work on toFormatString for Solve times

--- src/com/suushiemaniac/cubing/wca/result/Solve.java
+++ src/com/suushiemaniac/cubing/wca/result/Solve.java
@@ -1,6 +1,14 @@
 package com.suushiemaniac.cubing.wca.result;
 
 import com.suushiemaniac.cubing.wca.comp.Event;
+import com.suushiemaniac.cubing.wca.util.ArrayUtils;
+import com.suushiemaniac.cubing.wca.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solve {
     public static Solve fromFormatString() {
@@ -9,14 +17,32 @@ public class Solve {
     }
 
     public static String formatTime(long elapsed) {
-        return String.valueOf(elapsed);
+        //Conversions: centiseconds, seconds, minutes, hours
+        int[] convConst = {100, 60, 60, 24};
+        int[] formatValues = new int[convConst.length];
+
+        for (int i = 0; i < (convConst.length & formatValues.length); i++) {
+            if (i > 0) elapsed /= convConst[i - 1];
+            formatValues[i] = (int) (elapsed % convConst[i]);
+        }
+
+        int[] trimmed = ArrayUtils.autobox(ArrayUtils.trimBack(ArrayUtils.autobox(formatValues), 0));
+
+        //TODO korrekte Trennzeichen, alles außer erster Stelle mit 0 auffüllen
+        return String.join(".", ArrayUtils.toStringArray(ArrayUtils.reverse(ArrayUtils.autobox(trimmed))));
     }
 
     private long elapsed;
 
+    private boolean isAverage;
+
     private Event event;
 
     public Solve(long elapsed, Event event) {
+        this(elapsed, event, false);
+    }
+
+    public Solve(long elapsed, Event event, boolean isAverage) {
         this.elapsed = elapsed;
         this.event = event;
     }
@@ -29,12 +55,20 @@ public class Solve {
         return event;
     }
 
+    public boolean isAverage() {
+        return isAverage;
+    }
+
     public void setElapsed(long elapsed) {
         this.elapsed = elapsed;
     }
 
     public void setEvent(Event event) {
         this.event = event;
+    }
+
+    public void setIsAverage(boolean average) {
+        isAverage = average;
     }
 
     public String toFormatString() {
@@ -44,17 +78,43 @@ public class Solve {
         switch (timeFormat) {
             case "time":
                 return formatTime(this.elapsed);
+
             case "number":
-                return String.valueOf(this.elapsed);
+                return String.valueOf(this.isAverage ? ((float) this.elapsed) / 100 : this.elapsed);
+
             case "multi":
+
                 switch (this.event.getId()) {
                     case "333mbo":
-                        return "1SSAATTTTT";
+                        String oldValid = StringUtils.fillLeading("" + this.elapsed, '1', 10);
+                        Matcher oldMatcher = Pattern.compile("1(\\d{2})(\\d{2})(\\d{5})").matcher(oldValid);
+
+                        if (oldMatcher.find()) {
+                            int solved = 99 - Integer.parseInt(oldMatcher.group(1));
+                            int attempted = Integer.parseInt(oldMatcher.group(2));
+                            int seconds = Integer.parseInt(oldMatcher.group(3));
+
+                            return solved + "/" + attempted + " " + formatTime(seconds * 100);
+                        } else return "Invalid old multi-blind format";
+
                     case "333mbf":
-                        return "0DDTTTTTMM";
+                        String newValid = StringUtils.fillLeading("" + this.elapsed, '0', 10);
+                        Matcher newMatcher = Pattern.compile("0(\\d{2})(\\d{5})(\\d{2})").matcher(newValid);
+
+                        if (newMatcher.find()) {
+                            int difference = 99 - Integer.parseInt(newMatcher.group(1));
+                            int seconds = Integer.parseInt(newMatcher.group(2));
+                            int missed = Integer.parseInt(newMatcher.group(3));
+                            int solved = difference + missed;
+                            int attempted = solved + missed;
+
+                            return solved + "/" + attempted + " " + formatTime(seconds * 100);
+                        } else return "Invalid new multi-blind format";
+
                     default:
                         return "Invalid multi-blind discipline!";
                 }
+
             default:
                 return "Invalid time format!";
         }
--- src/com/suushiemaniac/cubing/wca/util/ArrayUtils.java
+++ src/com/suushiemaniac/cubing/wca/util/ArrayUtils.java
@@ -2,11 +2,10 @@ package com.suushiemaniac.cubing.wca.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static javafx.scene.input.KeyCode.T;
-
-public class ArrayUtils {
+public abstract class ArrayUtils {
     public static final String[] APLHABET = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 
     public static <T> T[] merge(T[] first, T[] second) {
@@ -26,16 +25,35 @@ public class ArrayUtils {
     }
 
     public static <T> T[] trim(T[] array, T element) {
-        List<T> trimList = new ArrayList<>();
+        return trimBack(trimFront(array, element), element);
+    }
 
-        int i, j;
+    public static <T> T[] trimFront(T[] array, T element) {
+        int i;
 
         for (i = 0; i < array.length; i++) if (!array[i].equals(element)) break;
-        for (j = array.length - 1; j >= 0; j--) if (!array[j].equals(element)) break;
 
-        trimList.addAll(Arrays.asList(array).subList(i, j + 1));
+        return subArray(array, i);
+    }
 
-        return trimList.toArray(Arrays.copyOf(array, trimList.size()));
+    public static <T> T[] trimBack(T[] array, T element) {
+        int i;
+
+        for (i = array.length - 1; i >= 0; i--) if (!array[i].equals(element)) break;
+
+        return subArray(array, 0, i + 1);
+    }
+
+    public static <T> T[] subArray(T[] array, int from, int to) {
+        return Arrays.asList(array).subList(from, to).toArray(Arrays.copyOf(array, to - from));
+    }
+
+    public static <T> T[] subArray(T[] array, int from) {
+        return subArray(array, from, array.length);
+    }
+
+    public static <T> List<T> toTypedList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
     }
 
     public static Integer[] autobox(int[] in) {
@@ -49,8 +67,28 @@ public class ArrayUtils {
     public static int[] autobox(Integer[] in) {
         int[] out = new int[in.length];
 
-        for (int i = 0; i < in.length; i++) out[i] = in[i];
+        for (int i = 0; i < in.length; i++)
+            out[i] = in[i];
 
         return out;
+    }
+
+    public static <T> String[] toStringArray(T[] in) {
+        String[] toReturn = new String[in.length];
+
+        for (int i = 0; i < (in.length & toReturn.length); i++) {
+            toReturn[i] = in[i].toString();
+        }
+
+        return toReturn;
+    }
+
+    public static <T> T[] reverse(T[] in) {
+        List<T> revList = new ArrayList<>();
+
+        Collections.addAll(revList, in);
+        Collections.reverse(revList);
+
+        return revList.toArray(Arrays.copyOf(in, in.length));
     }
 }
